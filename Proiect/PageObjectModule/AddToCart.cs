fc1fb4ed79928cd36502cfa2964ba76e3737282b
Vlad-Mihai133 <vladmihai.ionescu133@gmail.com>
2025-05-27 10:57:43 +0300

    refactor SearchTest

---
+++ Proiect/PageObjectModule/AddToCart.cs
@@ -0,0 +1,102 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ProiectCentric
+{
+    public class AddToCart
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public AddToCart(IWebDriver driver, WebDriverWait wait)
+        {
+            this.driver = driver;
+            this.wait = wait;
+        }
+        public void SearchAndAddProductToCart(string keyword = "arduino", string desiredProduct = "Arduino MEGA 2560")
+        {
+            this.WaitForCookies();
+
+            this.SearchProduct(keyword);
+
+            this.SelectProductFromResults(desiredProduct);
+
+            this.AddProductToCart();
+
+            this.ClickRequestOrderButton();
+
+            this.VerifyProductInCart();
+        }
+        private void WaitForCookies()
+        {
+            try
+            {
+                var acceptCookies = wait.Until(d => d.FindElement(By.Id("lgcookieslaw_accept")));
+                if (acceptCookies.Displayed && acceptCookies.Enabled)
+                {
+                    acceptCookies.Click();
+                    Console.WriteLine(" Accept cookies apăsat.");
+                }
+            }
+            catch (WebDriverTimeoutException)
+            {
+                Console.WriteLine(" Butonul de cookies NU a apărut.");
+            }
+        }
+        private void SearchProduct(string keyword)
+        {
+            var searchInput = wait.Until(d => d.FindElement(By.Name("search_query")));
+            searchInput.SendKeys(keyword);
+            searchInput.SendKeys(Keys.Enter);
+            Console.WriteLine($" Căutare '{keyword}' efectuată.");
+        }
+        private void SelectProductFromResults(string desiredProduct)
+        {
+            wait.Until(d => d.FindElements(By.CssSelector(".product_list .product-container .product-name")).Count > 0);
+            var products = driver.FindElements(By.CssSelector(".product_list .product-container .product-name"));
+
+            IWebElement targetProduct = products.FirstOrDefault(p => p.Text.Contains(desiredProduct));
+            Assert.IsNotNull(targetProduct, $" Produsul '{desiredProduct}' NU a fost găsit.");
+            Console.WriteLine(" Produsul a fost găsit.");
+
+            targetProduct.Click();
+        }
+        private void AddProductToCart()
+        {
+            var addToCart = wait.Until(d => d.FindElement(By.Id("add_to_cart")));
+            Assert.IsTrue(addToCart.Displayed, " Butonul 'Adaugă în coș' NU este vizibil.");
+            addToCart.Click();
+            Console.WriteLine(" Click pe 'Adaugă în coș'.");
+        }
+        private void ClickRequestOrderButton()
+        {
+            var requestOrder = wait.Until(d =>
+            {
+                var buttons = d.FindElements(By.CssSelector("a.btn"));
+                foreach (var btn in buttons)
+                {
+                    if (btn.Text.Trim().Contains("Solicitare comandă"))
+                        return btn;
+                }
+                throw new NoSuchElementException("Butonul 'Solicitare comandă' NU a fost găsit.");
+            });
+            requestOrder.Click();
+            Console.WriteLine(" Click pe 'Solicitare comandă'.");
+        }
+        private void  VerifyProductInCart()
+            {
+                var cartItems = wait.Until(d => d.FindElements(By.CssSelector(".cart_description .product-name")));
+                bool found = cartItems.Any(item => item.Text.Contains("Arduino MEGA 2560"));
+                Assert.IsTrue(found, " Produsul NU este prezent în coș.");
+                Console.WriteLine("Produsul se află în coș.");
+            }
+
+    }
+}
--- Proiect/UnitTest1.cs
+++ Proiect/UnitTest1.cs
@@ -5,13 +5,14 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System.Linq;
 
-namespace Proiect
+namespace ProiectCentric
 {
     [TestClass]
     public class PurchaseFlowTest
     {
         private IWebDriver driver;
         private WebDriverWait wait;
+        private AddToCart addToCart;
 
         [TestInitialize]
         public void SetUp()
@@ -19,6 +20,10 @@ namespace Proiect
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            addToCart = new AddToCart(driver, wait);
+
+            driver.Navigate().GoToUrl("https://www.optimusdigital.ro/");
+            Console.WriteLine(" Navigat la site.");
         }
 
         [TestCleanup]
@@ -30,63 +35,10 @@ namespace Proiect
         [TestMethod]
         public void SearchAndAddProductToCart()
         {
-            driver.Navigate().GoToUrl("https://www.optimusdigital.ro/");
-            Console.WriteLine(" Navigat la site.");
-
-            
-            try
-            {
-                var acceptCookies = wait.Until(d => d.FindElement(By.Id("lgcookieslaw_accept")));
-                if (acceptCookies.Displayed && acceptCookies.Enabled)
-                {
-                    acceptCookies.Click();
-                    Console.WriteLine(" Accept cookies apăsat.");
-                }
-            }
-            catch (WebDriverTimeoutException)
-            {
-                Console.WriteLine(" Butonul de cookies NU a apărut.");
-            }
-
-            
-            var searchInput = wait.Until(d => d.FindElement(By.Name("search_query")));
-            searchInput.SendKeys("arduino");
-            searchInput.SendKeys(Keys.Enter);
-            Console.WriteLine(" Căutare 'arduino' efectuată.");
-
-            wait.Until(d => d.FindElements(By.CssSelector(".product_list .product-container .product-name")).Count > 0);
-            var products = driver.FindElements(By.CssSelector(".product_list .product-container .product-name"));
-
-            IWebElement targetProduct = products.FirstOrDefault(p => p.Text.Contains("Arduino MEGA 2560"));
-            Assert.IsNotNull(targetProduct, " Produsul 'Arduino MEGA 2560' NU a fost găsit.");
-            Console.WriteLine(" Produsul a fost găsit.");
-
-            targetProduct.Click();
-
-            var addToCart = wait.Until(d => d.FindElement(By.Id("add_to_cart")));
-            Assert.IsTrue(addToCart.Displayed, " Butonul 'Adaugă în coș' NU este vizibil.");
-            addToCart.Click();
-            Console.WriteLine(" Click pe 'Adaugă în coș'.");
-
-            
-            var requestOrder = wait.Until(d =>
-            {
-                var buttons = d.FindElements(By.CssSelector("a.btn"));
-                foreach (var btn in buttons)
-                {
-                    if (btn.Text.Trim().Contains("Solicitare comandă"))
-                        return btn;
-                }
-                throw new NoSuchElementException("Butonul 'Solicitare comandă' NU a fost găsit.");
-            });
-            requestOrder.Click();
-            Console.WriteLine(" Click pe 'Solicitare comandă'.");
-
-            
-            var cartItems = wait.Until(d => d.FindElements(By.CssSelector(".cart_description .product-name")));
-            bool found = cartItems.Any(item => item.Text.Contains("Arduino MEGA 2560"));
-            Assert.IsTrue(found, " Produsul NU este prezent în coș.");
-            Console.WriteLine("Produsul se află în coș.");
+            string keyWord = "arduino";
+            string desiredProduct = "Arduino MEGA 2560";
+            this.addToCart.SearchAndAddProductToCart(keyWord, desiredProduct);
         }
+
     }
 }
